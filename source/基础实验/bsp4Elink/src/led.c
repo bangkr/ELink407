86227c93040f5f5c8a0970c2f2fda04cb99206f5
bangkr <bangkr@foxmail.com>
2015-09-24 17:35:42 +0800

    i

--- source/基础实验/bsp4Elink/src/led.c
+++ source/基础实验/bsp4Elink/src/led.c
@@ -9,6 +9,17 @@ void init_led(void)
 	/* Enable the GPIO_LED Clock */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	
+	//
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 |GPIO_Pin_13 |GPIO_Pin_14|GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	//GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	/* Configure the GPIO_LED pin */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
@@ -27,17 +38,27 @@ void init_led(void)
 void led_on(int ledx)
 {
 	if(LED1==ledx)
-		GPIO_ResetBits(GPIOA,GPIO_Pin_8);//点亮LED1 
+		//GPIO_ResetBits(GPIOA,GPIO_Pin_8);//点亮LED1 
+	GPIO_ResetBits(GPIOD,GPIO_Pin_12);//点亮LED1 
 	if(LED2==ledx)
-		GPIO_ResetBits(GPIOB,GPIO_Pin_8);//点亮LED2
+		//GPIO_ResetBits(GPIOB,GPIO_Pin_8);//点亮LED2
+	GPIO_ResetBits(GPIOD,GPIO_Pin_13);//点亮LED1 
 }
 
 void led_off(int ledx)
 {
+	/*
 	if(LED1==ledx)
 		GPIO_SetBits(GPIOA,GPIO_Pin_8);//关闭LED1 
 	if(LED2==ledx)
 		GPIO_SetBits(GPIOB,GPIO_Pin_8);//关闭LED2
+	*/
+	if(LED1==ledx)
+		//GPIO_ResetBits(GPIOA,GPIO_Pin_8);//点亮LED1 
+	GPIO_SetBits(GPIOD,GPIO_Pin_12);//点亮LED1 
+	if(LED2==ledx)
+		//GPIO_ResetBits(GPIOB,GPIO_Pin_8);//点亮LED2
+	GPIO_SetBits(GPIOD,GPIO_Pin_13);//点亮LED1 
 	
 }
 
